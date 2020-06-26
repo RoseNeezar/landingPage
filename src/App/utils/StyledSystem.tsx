ae6522ffba04ee68ef2801e258bbea75edbf5f44
Neezar <rforlyffe@gmaicl.com>
2020-06-26 15:44:13 +0800

    add navigation to main

---
+++ src/App/utils/StyledSystem.tsx
@@ -0,0 +1,80 @@
+import React, { ReactNode } from "react";
+import styled from "styled-components";
+
+import {
+  compose,
+  buttonStyle,
+  space,
+  SpaceProps,
+  color,
+  ColorProps,
+  layout,
+  LayoutProps,
+  typography,
+  TypographyProps,
+  flexbox,
+  FlexboxProps,
+  border,
+  BorderProps,
+  background,
+  BackgroundProps,
+  position,
+  PositionProps,
+  grid,
+  GridProps,
+  shadow,
+  ShadowProps,
+  width,
+  WidthProps,
+  minWidth,
+  MinWidthProps,
+  height,
+  HeightProps,
+  minHeight,
+  MinHeightProps,
+  ButtonStyleProps,
+} from "styled-system";
+
+export type StyledSystemProps =
+  | SpaceProps
+  | ButtonStyleProps
+  | LayoutProps
+  | TypographyProps
+  | ColorProps
+  | FlexboxProps
+  | BorderProps
+  | BackgroundProps
+  | PositionProps
+  | GridProps
+  | ShadowProps
+  | WidthProps
+  | MinWidthProps
+  | HeightProps
+  | MinHeightProps
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  | {
+      color: string;
+      as?: keyof JSX.IntrinsicElements | React.ComponentType<any> | ReactNode;
+    };
+
+export const styledSystemC = (tag: StyledSystemProps | any) => {
+  return styled(tag)(
+    compose(
+      space,
+      buttonStyle,
+      color,
+      layout,
+      typography,
+      flexbox,
+      border,
+      background,
+      position,
+      grid,
+      shadow,
+      width,
+      minWidth,
+      height,
+      minHeight
+    )
+  );
+};
