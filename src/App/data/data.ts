c916b51486f2b52e212378035922a0cb7d5852cc
Neezar <rforlyffe@gmaicl.com>
2020-06-28 17:00:29 +0800

    add logo list

--- src/App/data/data.ts
+++ src/App/data/data.ts
@@ -33,3 +33,22 @@ export const BrandList = [
     links: "https://www.dooexchange.com/",
   },
 ];
+
+export const LogoList = [
+  {
+    id: 1,
+    url: "https://www.doo.hk/static/media/DooPrime.4925b672.svg",
+  },
+  {
+    id: 2,
+    url: "https://www.doo.hk/static/media/DooClearing.c10e1125.svg",
+  },
+  {
+    id: 3,
+    url: "https://www.doo.hk/static/media/DooTech.e98c2c6d.svg",
+  },
+  {
+    id: 4,
+    url: "https://www.doo.hk/static/media/DooExchange.c93d73af.svg",
+  },
+];
